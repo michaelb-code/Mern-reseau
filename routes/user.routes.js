89179817f3d95970263534255fa813138c1ac09d
michaelb-code <biganzolimichael@gmail.com>
2025-11-05 18:01:13 +0100

    ajout des routes follow/unfollow

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,10 +3,14 @@ const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
 
 // AUTH
-router.post("/register", authController.signUp);
+router.post("/register", authController.signUp); // Création d'un utilisateur
 
 // USER DB
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.userInfo)
+router.get("/", userController.getAllUsers); // recuperer tous les utilisateurs
+router.get("/:id", userController.userInfo); // recuperer un utilisateur
+router.put("/:id", userController.updateUser);// modifier un utilisateur
+router.delete("/:id", userController.deleteUser); // supprimer un utilisateur
+router.patch("/follow/:id", userController.followUser);
+router.patch("/unfollow/:id", userController.unfollowUser);
 
 module.exports = router;
